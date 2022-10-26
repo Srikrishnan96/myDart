d444d4877230c977945c8b30027ed98b4e2e78ba
Srikrishnan Chennai <schennai@tekion.com>
2022-10-25 21:37:59 -0700

    start dart

---
+++ project_beginning/bin/project_beginning.dart
@@ -0,0 +1,5 @@
+import 'package:project_beginning/project_beginning.dart' as project_beginning;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_beginning.calculate()}!');
+}
---
+++ project_beginning/test/project_beginning_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_beginning/project_beginning.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
